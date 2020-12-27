c771f288495e60d49897044b246da600a24cdc64
Jan Müller <janmueller3698@gmail.com>
2020-12-27 12:12:01 +0100

    FOL: ModelCheckerTrace contains the String representation of formulas

--- src/eu/yeger/gramofo/fol/graph/ModelCheckerTrace.kt
+++ src/eu/yeger/gramofo/fol/graph/ModelCheckerTrace.kt
@@ -3,7 +3,7 @@ package eu.yeger.gramofo.fol.graph
 import eu.yeger.gramofo.fol.formula.FOLFormula
 
 data class ModelCheckerTrace(
-    val formula: FOLFormula,
+    val formula: String,
     val text: String,
     val isModel: Boolean,
     val children: List<ModelCheckerTrace>
@@ -17,8 +17,8 @@ fun List<ModelCheckerTrace>.split(): Pair<List<ModelCheckerTrace>, List<ModelChe
     }
 }
 
-fun FOLFormula.validated(text: String, children: List<ModelCheckerTrace>) = ModelCheckerTrace(this, text, true, children.toList())
-fun FOLFormula.validated(text: String, vararg children: ModelCheckerTrace) = ModelCheckerTrace(this, text, true, children.toList())
+fun FOLFormula.validated(text: String, children: List<ModelCheckerTrace>) = ModelCheckerTrace(this.toString(), text, true, children.toList())
+fun FOLFormula.validated(text: String, vararg children: ModelCheckerTrace) = ModelCheckerTrace(this.toString(), text, true, children.toList())
 
-fun FOLFormula.invalidated(text: String, children: List<ModelCheckerTrace>) = ModelCheckerTrace(this, text, false, children.toList())
-fun FOLFormula.invalidated(text: String, vararg children: ModelCheckerTrace) = ModelCheckerTrace(this, text, false, children.toList())
+fun FOLFormula.invalidated(text: String, children: List<ModelCheckerTrace>) = ModelCheckerTrace(this.toString(), text, false, children.toList())
+fun FOLFormula.invalidated(text: String, vararg children: ModelCheckerTrace) = ModelCheckerTrace(this.toString(), text, false, children.toList())

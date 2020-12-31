ff61ce1cf8b3fca00c1e47b8e7b27881c36f93b8
Jan Müller <janmueller3698@gmail.com>
2020-12-31 12:40:08 +0100

    Parser: Cleanup

--- src/eu/yeger/gramofo/fol/parser/ParseException.kt
+++ src/eu/yeger/gramofo/fol/parser/ParseException.kt
@@ -1,9 +1,3 @@
 package eu.yeger.gramofo.fol.parser
 
-/**
- * This class is used to represent a exception while parsing a sequence. The
- * index is used to determine the position of the input array, where the error
- * occurred to create useful error messages.
- *
- */
 class ParseException(override val message: String) : Exception(message)

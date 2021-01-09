ce93a7130f0f0437ebf85bf09d43b874e3c568d4
Jan Müller <janmueller3698@gmail.com>
2021-01-09 19:09:27 +0100

    Ktor: Handle OutOfMemoryError during serialization

--- src/eu/yeger/gramofo/utils/KtorUtils.kt
+++ src/eu/yeger/gramofo/utils/KtorUtils.kt
@@ -6,11 +6,16 @@ import eu.yeger.gramofo.model.api.ApiResult
 import eu.yeger.gramofo.model.api.HttpEntity
 import eu.yeger.gramofo.model.dto.TranslationDTO
 import io.ktor.application.*
+import io.ktor.http.*
 import io.ktor.response.*
 
 suspend fun <T : Any> ApplicationCall.respondWithResult(result: ApiResult<T>) {
-    when (result) {
-        is Ok<HttpEntity<T>> -> respond(result.value.status, result.value.data)
-        is Err<HttpEntity<TranslationDTO>> -> respond(result.error.status, mapOf("message" to result.error.data))
+    try {
+        when (result) {
+            is Ok<HttpEntity<T>> -> respond(result.value.status, result.value.data)
+            is Err<HttpEntity<TranslationDTO>> -> respond(result.error.status, mapOf("message" to result.error.data))
+        }
+    } catch (outOfMemoryError: OutOfMemoryError) {
+        respond(HttpStatusCode.InsufficientStorage, mapOf("message" to "api.error.response-too-big"))
     }
 }

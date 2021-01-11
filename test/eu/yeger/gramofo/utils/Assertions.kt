38f38fc8b44606b660c873a110ece91c974cdbd3
Jan Müller <janmueller3698@gmail.com>
2021-01-11 19:39:29 +0100

    ModelChecker: Updated tests to validate implementation of partialCheck

--- test/eu/yeger/gramofo/utils/Assertions.kt
+++ test/eu/yeger/gramofo/utils/Assertions.kt
@@ -8,3 +8,8 @@ infix fun Any?.shouldBe(expected: Any?) =
 
 infix fun Any?.shouldNotBe(illegal: Any?) =
     assertNotEquals(illegal = illegal, actual = this)
+
+fun <T> checkRecursive(current: T, getNext: (T) -> List<T>?, check: (T) -> Unit) {
+    check(current)
+    getNext(current)?.forEach { next -> checkRecursive(next, getNext, check) }
+}
